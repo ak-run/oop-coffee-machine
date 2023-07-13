7075702631413975b47ad009aed4873d7ae9450a
ak-run <123417735+ak-run@users.noreply.github.com>
2023-07-13 10:51:11 +0100

    adding comments

--- main.py
+++ main.py
@@ -8,15 +8,23 @@ coffee_machine = CoffeeMaker()
 money_machine = MoneyMachine()
 
 while is_on:
+    # asking user to choose a drink
     choice = input(f"What would you like? {coffee_menu.get_items()}: ")
+    # actions based on answer
+    # turns machine off
     if choice == "off":
         is_on = False
+    # prints report
     elif choice == "report":
         coffee_machine.report()
         money_machine.report()
     else:
         drink = coffee_menu.find_drink(choice)
-        if drink != None and coffee_machine.is_resource_sufficient(drink):
+        # if drink on the menu and resource sufficient machine asks for payment
+        if drink is not None and coffee_machine.is_resource_sufficient(drink):
             cost = drink.cost
             money_machine.make_payment(cost)
+            """if payment successful makes a drink"""
             coffee_machine.make_coffee(drink)
+    # if choice not on the menu, user is prompted for input again"""
+
